3937795eff774a033793f94713f2190d7157b46d
Vicente Santiago <vicentejsp@outlook.com>
2019-12-17 18:05:29 -0600

    Actualizando

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
     console.log(`Servidor corriendo en el puerto:${3000}`);
